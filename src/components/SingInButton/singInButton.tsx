e306fdbf3781891bb7baa2acbfc5f5493bc03c85
beatriz de souza meneses <beatriz.workspace@gmail.com>
2023-01-13 18:06:12 -0300

    session.data

--- src/components/SingInButton/singInButton.tsx
+++ src/components/SingInButton/singInButton.tsx
@@ -10,7 +10,7 @@ export function SingInButton(){
 
     console.log(session)
 
-    return session.status == "authenticated" ? (
+    return session.data? (
         <button type="button" className={styles.singInButton} onClick={()=> signOut()}>
             <FaGithub color="#04d361"/>
             {session.data.user?.name}
